e169e2b82259243769b16fd86c23ee690fee0104
Rajesh Das <rajesh@hyperoot.dev>
2026-03-08 01:04:25 +0000

    feat: add core components including Header, Footer, and Mobile Menu; implement UI components like Button, Badge, Input, Label, Textarea, and ImageCarousel; create 404 page and component documentation; enhance RSS feed generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "astro-auto-import";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://rajeshdas.dev",
   prefetch: true,
   vite: {
     plugins: [tailwindcss()],
   },
+  markdown: {
+    shikiConfig: {
+      theme: "css-variables",
+      defaultColor: false,
+    },
+  },
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+    AutoImport({
+      imports: ["./src/components/core/Video.astro"],
+    }),
+    mdx(),
+  ],
 });
